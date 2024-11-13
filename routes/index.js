30fe8b24454237e9ed9288066e82af3c93e0d5b0
alth44f <althafpnlm@gmail.com>
2024-11-13 15:28:13 +0530

    signup added

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Users' });
+  res.render('index', { title: 'RegEx' });
 });
 router.get('/login', function(req, res) {
   res.render('login', { title: 'Users' });
 });
 router.post('/login',(req,res)=>{
   console.log(req.body.password)
-  res.render('userDashboard',{user: req.body.email})
+  res.render('userDashboard',{user: req.body.email,login: true})
 })
 router.get('/signup', function(req, res) {
   res.render('signup');
 });
+router.post('/signup', function(req, res) {
+  console.log(req.body);
+  
+});
+
 module.exports = router;
