40c80da50585e07c357d2ff027dba5caa6bc0574
anagh534 <anaghkrkkl@gmail.com>
2024-11-20 16:21:08 +0530

    hod verifylogin

--- routes/hod.js
+++ routes/hod.js
@@ -2,15 +2,35 @@ var express = require('express');
 var router = express.Router();
 const hodHelper = require('../helper/hodHelper');
 
-router.get('/', (req, res) => {
+const verifyLogin = (req, res, next) => {
+  if (req.session.hodLoggedIn) {
+    next()
+  } else {
+    res.redirect('/hod/login')
+  }
+}
+
+router.get('/', verifyLogin, (req, res) => {
   let data = {
     reg: 122,
     nonreg: 56
   }
-  res.render('hod/dashboard', {hod: {name: 'Admin'}, data })
+  res.render('hod/dashboard', { hod: { name: 'Admin' }, data, err: req.session.hodLoginErr })
 });
 router.get('/login', (req, res) => {
-  res.render('login')
+  res.render('hod/login')
 })
-
+router.post('/login', (req, res) => {
+  hodHelper.doLogin(req.body).then((resp) => {
+    console.log(resp)
+    if (resp.err) {
+      req.session.hodLoginErr = resp.err
+      res.redirect('/hod/login')
+    } else {
+      req.session.hodLoggedIn = true
+      req.session.hodLoginErr = null
+      res.redirect('/hod')
+    }
+  })
+})
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 let studentHelper = require('../helper/studentHelper')
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'RegEx' });
+  res.render('index', { title: 'RegEx', student: true });
 });
 router.get('/login', function (req, res) {
-  res.render('login', { title: 'Users', err: req.session.loginErr });
+  res.render('login', { title: 'Users', err: req.session.loginErr, student: true });
 });
 router.post('/login', (req, res) => {
   studentHelper.doLogin(req.body).then((resp) => {
@@ -16,20 +15,20 @@ router.post('/login', (req, res) => {
       res.redirect('/login')
     } else {
       req.session.loginErr = null
-      res.render('userDashboard', { user: resp.user[0].name, login: true })
+      res.render('userDashboard', { user: resp.user[0].name, login: true, student: true })
     }
   })
 })
 router.get('/signup', function (req, res) {
-  res.render('signup');
+  res.render('signup', { student: true });
 });
 router.post('/signup', function (req, res) {
   studentHelper.doSignup(req.body).then(() => {
     res.redirect('/login')
   })
 });
-router.get('/register-exam',(req,res)=>{
-  res.render('registerExam.hbs')
+router.get('/register-exam', (req, res) => {
+  res.render('registerExam.hbs', { student: true })
 })
 
 module.exports = router;
